6430536cbe3dc7494ae93b0a9810462f91d86c4d
Prasanna <Prasanna@DESKTOP-VIB6UCD>
2022-01-20 00:14:50 +0530

    20/01/2021 git test

---
+++ src/test/java/gitTest/newTest.java
@@ -0,0 +1,10 @@
+package gitTest;
+
+public class newTest {
+	public static void main(String[] args) {
+		System.out.println("Hi my git practice");
+		System.out.println("my git practice is working");
+		System.out.println("hi welcome baby");
+	}
+
+}
